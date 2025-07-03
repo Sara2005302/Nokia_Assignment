8d84cd1eed80434d538cb191edbbdc347461fa8c
Sara2005302 <sara2005nya@gmail.com>
2025-07-03 23:21:11 +0530

    Delete src/src directory

--- src/src/embedder.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-def generate_embeddings_from_chunks(chunks, model_name="all-MiniLM-L6-v2"):
-    model = SentenceTransformer(model_name)
-    embeddings = model.encode(chunks, convert_to_numpy=True)
-    return embeddings
